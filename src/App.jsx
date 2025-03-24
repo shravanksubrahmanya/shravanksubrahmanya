1f8c0688f5d4357fb4083c48e00bfd16a732c261
Shravan K Subrahmanya <shravanksubrahmanya@gmail.com>
2025-03-24 13:44:06 +0530

    add Contact section with email form; integrate EmailJS for message sending

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/sections/Home";
 import { About } from "./components/sections/About";
 import { Projects } from "./components/sections/Projects";
+import { Contact } from "./components/sections/Contact";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -26,6 +27,7 @@ function App() {
         <Home />
         <About />
         <Projects />
+        <Contact />
       </div>
     </>
   );
