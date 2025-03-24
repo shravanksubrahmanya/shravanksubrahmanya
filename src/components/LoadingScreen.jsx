37f31f62624f801d23968f14c3e5d8bacf911a5b
Shravan K Subrahmanya <shravanksubrahmanya@gmail.com>
2025-03-24 15:16:20 +0530

    remove .env file and update LoadingScreen text to enhance user experience

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState("");
-  const fulltext = "<Hello World />";
+  const fulltext = "print('Thanks for visiting—let’s explore!')";
 
   useEffect(() => {
     let index = 0;
