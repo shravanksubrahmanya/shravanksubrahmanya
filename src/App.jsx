3b9db4ad693c240982fa15cefb21f2b954851c5b
Shravan K Subrahmanya <shravanksubrahmanya@gmail.com>
2025-03-24 11:15:16 +0530

    add About section component to App

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/sections/Home";
+import { About } from "./components/sections/About";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -22,6 +23,7 @@ function App() {
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
+        <About />
       </div>
     </>
   );
