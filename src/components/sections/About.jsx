9fb6cb22554b4c5e07ab947a0a6493d5e83f2abb
Shravan K Subrahmanya <shravanksubrahmanya@gmail.com>
2025-03-24 18:26:04 +0530

    enhance responsiveness and styling: update CSS for better mobile support and adjust layout in Home and Projects sections

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -26,22 +26,21 @@ export const About = () => {
   return (
     <section
       id="about"
-      className="min-h-screen flex items-center justify-center py-20"
+      className="min-h-screen flex items-center justify-center py-20 px-4"
     >
       <RevealOnScroll>
-        <div className="max-w-3xl mx-auto px-4">
-          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+        <div className="max-w-4xl mx-auto">
+          <h2 className="text-3xl sm:text-4xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
             About Me
           </h2>
-          <div className="glass rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+          <div className="glass rounded-xl p-6 sm:p-8 border-white/10 border hover:-translate-y-1 transition-all">
             <p className="text-gray-300 mb-6 text-center">
               Innovative Software Engineer and Microsoft Certified Azure
               Developer Associate with expertise in Full-Stack Web Development,
               specializing in React.js, Node.js, Python, and Azure AI. Skilled
               in crafting user-friendly interfaces and designing scalable
-              architectures
+              architectures.
             </p>
-            {/* skills */}
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <div className="rounded-xl hover:translate-y-1 transition-all">
                 <h3 className="text-xl font-bold mb-4">Frontend</h3>
@@ -49,8 +48,7 @@ export const About = () => {
                   {frontendSkills.map((tech, key) => (
                     <span
                       key={key}
-                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
                     >
                       {tech}
                     </span>
@@ -63,79 +61,11 @@ export const About = () => {
                   {backendSkills.map((tech, key) => (
                     <span
                       key={key}
-                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
-                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
                     >
                       {tech}
                     </span>
                   ))}
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/* education */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
-            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-boldmb-4 ">🏫 Education</h3>
-              <ul className="list-disc list-inside text-gray-300 space-y-2">
-                <li>
-                  <strong>Master of Computer Applications (MCA)</strong> -
-                  Ramaiah Institute of Technology (2021-2023)
-                </li>
-                <li>
-                  <strong>Bachelor of Computer Applications (BCA)</strong> -
-                  Alva's Degree College, Moodabidri (2016-2019)
-                </li>
-              </ul>
-            </div>
-            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-boldmb-4 ">💼 Work Experience</h3>
-              <div className="space-y-4 text-gray-300">
-                <div>
-                  <h4 className="font-semibold">
-                    <strong>Software Engineer</strong> - Donyati India Private
-                    Ltd.(Sept, 2023 - Present)
-                  </h4>
-                  <p>
-                    At Donyati India Pvt Ltd, I specialize in developing
-                    solutions using React JS and Python, integrating Azure AI
-                    Services and OpenAI for AI driven applications. By
-                    leveraging Azure cloud services, I enhance productivity
-                    while focusing on both front-end and back-end development. I
-                    create dynamic, user-friendly interfaces and scalable
-                    architectures, collaborating with cross functional teams to
-                    deliver high-quality software solutions
-                  </p>
-                </div>
-                <div>
-                  <h4 className="font-semibold">
-                    <strong>Software Engineer Intern</strong> - Centre for Smart
-                    Governance, Karnataka(Feb, 2023 - May, 2023)
-                  </h4>
-                  <p>
-                    As an intern at the Centre for Smart Governance Karnataka, I
-                    gained hands-on experience with ASP.Net, MS-SQL, AJAX, and
-                    JavaScript. I developed web applications, honed my
-                    problem-solving skills, and learned to collaborate
-                    effectively with a team of developers. My primary project
-                    was the development of the "New Gandhi Sakchi Kayaka (NGSK)"
-                    application
-                  </p>
-                </div>
-                <div>
-                  <h4 className="font-semibold">
-                    <strong>Operations Executive</strong> - Infosys Limited(Jun
-                    2019 - Nov 2020)
-                  </h4>
-                  <p>
-                    During my tenure at Infosys Limited, I worked on JavaScript
-                    development for Power Pages, creating interactive and
-                    dynamic web pages to enhance user experience. I contributed
-                    to building scalable and efficient web solutions, and my
-                    major project involved developing Power Platform solutions
-                    for SunTrust Banks using Power Apps
-                  </p>
                 </div>
               </div>
             </div>
--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -4,31 +4,29 @@ export const Home = () => {
   return (
     <section
       id="home"
-      className="min-h-screen flex items-center justify-center relative"
+      className="min-h-screen flex items-center justify-center relative px-4"
     >
       <RevealOnScroll>
-        <div className="text-center z-10 px-4">
-          <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent learing-right">
+        <div className="text-center z-10">
+          <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold mb-4 md:mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent">
             Hi, I'm Shravan K Subrahmanya
           </h1>
-          <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
+          <p className="text-gray-400 text-sm sm:text-base md:text-lg mb-6 md:mb-8 max-w-lg mx-auto">
             Innovative Software Engineer and Microsoft Certified Azure Developer
             Associate with expertise in Full-Stack Web Development, specializing
             in React.js, Node.js, Python, and Azure AI. Skilled in crafting
-            user-friendly interfaces and designing scalable architectures
+            user-friendly interfaces and designing scalable architectures.
           </p>
-          <div className="flex justify-center space-x-4">
+          <div className="flex flex-wrap justify-center gap-4">
             <a
               href="#projects"
-              className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden 
-            hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
+              className="bg-blue-500 text-white py-2 sm:py-3 px-4 sm:px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
             >
               View Projects
             </a>
             <a
               href="#contact"
-              className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200
-            hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10"
+              className="border border-blue-500/50 text-blue-500 py-2 sm:py-3 px-4 sm:px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10"
             >
               Contact me
             </a>
