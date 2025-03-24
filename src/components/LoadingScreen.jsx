86c8573823b03351fa642e70544314ec69fcf9c6
Shravan K Subrahmanya <shravanksubrahmanya@gmail.com>
2025-03-24 19:24:37 +0530

    refactor LoadingScreen component: update loading text formatting and enhance layout for better responsiveness

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState("");
-  const fulltext = "print('Thanks for visiting—let’s explore!')";
+  const fulltext = "print('Thanks for visiting — let’s explore!')";
 
   useEffect(() => {
     let index = 0;
@@ -22,15 +22,13 @@ export const LoadingScreen = ({ onComplete }) => {
   }, [onComplete]);
 
   return (
-    <>
-      <div className="fixed inset-0 x-50 bg-black text-gray-100 flex flex-col items-center justify-center">
-        <div className="mb-4 text-4xl font-mono font-bold">
-          {text} <span className="animate-blink ml-1">|</span>
-        </div>
-        <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
-          <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
-        </div>
+    <div className="fixed inset-0 bg-black text-gray-100 flex flex-col items-center justify-center px-4">
+      <div className="mb-4 text-2xl sm:text-3xl md:text-4xl font-mono font-bold text-center">
+        {text} <span className="animate-blink ml-1">|</span>
       </div>
-    </>
+      <div className="w-full max-w-xs sm:max-w-sm h-[2px] bg-gray-800 rounded relative overflow-hidden">
+        <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
+      </div>
+    </div>
   );
 };
